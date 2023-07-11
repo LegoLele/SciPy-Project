7a51fc1c8b7f88294d45d38fdf0ca1ce9b2a7988
Melisa Altinyelek <melisaaltinyelek@hotmail.com>
2023-07-11 19:18:40 +0200

    Added comments

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pandas as pd
 import re
-from datetime import datetime
 
 
-# Opens the WhatsApp dataset in the read mode
 def read_data():
+    """
+    Reads the data, extracts timestamps, senders and messages, converts the data into dataframe.
+
+    Returns:
+        chat_df: The WhatsApp chat data is returned as a dataframe.
+    """ 
+
+    # Captures timestamp, sender and message based on the dataset
     regex = r"\[(.*?)\]\s(.*?):\s(.*)"
+
+    # Opens the WhatsApp dataset in the read mode
     with open(whatsapp_data_path, "r", encoding = "UTF-8") as file:
         txt_reader = file.read()
-        #print(line_reader)
+
+    # Finds all regex matches in the text file
     reg_match = re.findall(regex, txt_reader)
-        
+    
+    # Each message is stored as dictionaries
     chat_df = []
     for i in range(len(reg_match)):
         j = {
@@ -21,6 +31,7 @@ def read_data():
 
         chat_df.append(j)
 
+    # Dictionaries are appended to the chat_df list
     chat_df = pd.DataFrame(chat_df)
 
     return chat_df
