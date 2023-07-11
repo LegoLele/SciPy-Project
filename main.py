7240f87f7b26b65c79ab3ed148f289fa65b82acf
Melisa Altinyelek <melisaaltinyelek@hotmail.com>
2023-07-11 18:55:34 +0200

    regex, txt converted into df

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import pandas as pd
+import re
+from datetime import datetime
+
+
 # Opens the WhatsApp dataset in the read mode
 def read_data():
-    with open(whatsapp_data_path, "r") as file:
-        reader = file.read()
-        print(reader)
+    regex = r"\[(.*?)\]\s(.*?):\s(.*)"
+    with open(whatsapp_data_path, "r", encoding = "UTF-8") as file:
+        txt_reader = file.read()
+        #print(line_reader)
+    reg_match = re.findall(regex, txt_reader)
         
+    chat_df = []
+    for i in range(len(reg_match)):
+        j = {
+        "Time": reg_match[i][0],
+        "Sender": reg_match[i][1],
+        "Message": reg_match[i][2]
+        }
+
+        chat_df.append(j)
+
+    chat_df = pd.DataFrame(chat_df)
+
+    return chat_df
+    
 
 # Define a file path
 whatsapp_data_path = "Data/WhatsApp_data.txt"
-read_data()
+df = read_data()
+print(df)
