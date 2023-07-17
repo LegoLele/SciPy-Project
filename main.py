f02aeef9801141a7075f257299aee9b5a1c41a89
Leonartion <83833669+Leonartion@users.noreply.github.com>
2023-07-17 13:48:39 +0200

    Update main.py
    added a simply dataframe analysis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import re
 
-
 def read_data(whatsapp_data_path):
     """
     Reads the data, extracts timestamps, senders and messages, converts the data into dataframe.
@@ -38,10 +37,42 @@ def read_data(whatsapp_data_path):
     # chat_df list is converted into a dataframe
     chat_df = pd.DataFrame(chat_df)
 
+    #delete first two rows (meassages not sent by a human but by Whats App)
+    chat_df = chat_df[2:]
+
     return chat_df
     
+def simple_analysis(df):
+    #Most active time
+    df['Time'] = pd.to_datetime(df['Time'], format='%d.%m.%y, %H:%M:%S')
+    df['hour'] = df['Time'].dt.hour
+    hourly_counts = df.groupby('hour').size()
+    max_hour = hourly_counts.idxmax()
+    max_messages = hourly_counts[max_hour]
+    print(f"The most messages were sent between {max_hour} and {(max_hour+1)} o'clock ({max_messages} messages).")
 
-# Define a file path
-whatsapp_data_path = "Data/WhatsApp_data.txt"
-df = read_data(whatsapp_data_path)
-print(df)
+    #Least active time
+    min_hour = hourly_counts.idxmin()
+    min_messages = hourly_counts[min_hour]
+    print(f"The least messages were sent between {min_hour} and {(min_hour+1)} o'clock ({min_messages} messages).")
+
+    #Most active Author
+    sender_counts = df['Sender'].value_counts()
+    max_sender = sender_counts.idxmax()
+    max_messages = sender_counts[max_sender]
+    max_messages_percent = round(max_messages/len(df) * 100, 2)
+    print(f"The sender with the most messages is {max_sender} with {max_messages} messages ({max_messages_percent}%).")
+
+    #Least active Author
+    min_sender = sender_counts.idxmin()
+    min_messages = sender_counts[min_sender]
+    min_messages_percent = round(min_messages/len(df) * 100, 2)
+    print(f"The sender with the least messages is {min_sender} with {min_messages} messages ({min_messages_percent}%).")
+
+if __name__ == "__main__":
+    # Define a file path
+    whatsapp_data_path = "Data/WhatsApp_data.txt"
+
+    df = read_data(whatsapp_data_path)
+    print(df)
+    simple_analysis(df)
