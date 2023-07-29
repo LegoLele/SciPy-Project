823d7739725ad11d71a628e84f32564e3522b620
Leonartion <83833669+Leonartion@users.noreply.github.com>
2023-07-29 10:14:50 +0200

    arima

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import re
 import seaborn as sns
 from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.arima.model import ARIMA
 
 
 def read_data(whatsapp_data_path):
@@ -120,15 +121,88 @@ def activity_heatmap():
         plt.tight_layout()
         plt.show()
 
-def test_stationarity(df):
-    # Construct timeseries with meassage count
-    timeseries = df.groupby('Time').size()
+def prepare_data(df):
+    # Convert 'Time' column to datetime and set it as the index
+    df['Time'] = pd.to_datetime(df['Time'], format='%d.%m.%y, %H:%M:%S')
+    df.set_index('Time', inplace=True)
+
+    # Resample data to get messages per hour
+    messages_per_hour = df.resample('H').count()
+    messages_per_hour.rename(columns={'Message': 'MessagesCount'}, inplace=True)
+
+    # Drop unnessecary columns
+    messages_per_hour.drop(columns='Sender', inplace=True)
+
+    # Return DataFrame with messages per hour
+    return messages_per_hour
+
+def check_stationarity(data):
     # Perform Dickey-Fuller test
-    dftest = adfuller(timeseries, autolag='AIC')
-    result = pd.Series(dftest[0:4], index=['Test Statistic', 'p-value', 'Lags Used', 'Number of Observations Used'])
-    for key, value in dftest[4].items():
-        result[f'Critical Value ({key})'] = value
-    return result
+    result = adfuller(data, autolag='AIC')
+
+    # Print test results
+    print("Dickey-Fuller Test Results:")
+    print(f'Test Statistic: {result[0]}')
+    print(f'p-value: {result[1]}')
+    print(f'Critical Values:')
+    for key, value in result[4].items():
+        print(f'   {key}: {value}')
+
+    # Check if p-value is below threshhold (normally 0.05)
+    if result[1] <= 0.05:
+        print("Data is stationary (reject the null hypothesis).")
+        return True
+    else:
+        print("Data is non-stationary (fail to reject the null hypothesis).")
+        return False
+
+def fit_arima_model(data):
+    # Create time index for the data
+    time_index = pd.date_range(start=data.index[0], periods=len(data), freq=data.index.inferred_freq)
+
+    # Fit ARIMA model using the time index
+    model = ARIMA(data, order=(1, 0, 1), dates=time_index)  # Specify the dates parameter
+    model_fit = model.fit()
+
+    # Summary of the model
+    print(model_fit.summary())
+
+    return model_fit
+
+def make_predictions(model_fit, n_periods):
+    # Forecast n_periods into the future
+    forecast = model_fit.forecast(steps=n_periods)
+
+    # Get the last time point in the original data
+    last_time = model_fit.model.nobs - 1
+    last_time_point = model_fit.model.data.dates[last_time]
+
+    # Create a time index for the forecast
+    forecast_index = pd.date_range(start=last_time_point, periods=n_periods, freq=model_fit.model.data.freq)
+
+    # Create a DataFrame with the forecast
+    forecast_df = pd.DataFrame(data=forecast, index=forecast_index, columns=['Forecast'])
+
+    return forecast_df
+
+def predict_chat_messages(df, n_periods):
+    # Prepare data
+    df_arima = prepare_data(df)
+
+    # Check stationarity
+    is_stationary = check_stationarity(df_arima['MessagesCount'])
+
+    if not is_stationary:
+        print("Data is not stationary. Apply transformations to make it stationary.")
+        return
+
+    # Fit ARIMA model
+    model_fit = fit_arima_model(df_arima)
+
+    # Make predictions
+    forecast_df = make_predictions(model_fit, n_periods)
+
+    return forecast_df
 
 if __name__ == "__main__":
     # Define a file path
@@ -138,5 +212,4 @@ if __name__ == "__main__":
     print(df)
     text_analysis(df)
     activity_heatmap()
-    print(test_stationarity(df))
-    print("-> Since the Test Statistic is clearly more negative than all Critical values, we can safely consider our data as stationary and don't have to make it stationary.")
+    print(predict_chat_messages(df,20))
